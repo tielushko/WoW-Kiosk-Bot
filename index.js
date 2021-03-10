42f18974e17fc7735914be5d301451ec540fc362
Ivan Castanon <icastanon@usf.edu>
2021-03-09 21:56:38 -0500

    Modified index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const restify = require('restify');
 const { BotFrameworkAdapter } = require('botbuilder');
 
 // This bot's main dialog.
-//const { EchoBot } = require('./bot');
+const { EchoBot } = require('./bot');
 
 // Create HTTP server
 const server = restify.createServer();
